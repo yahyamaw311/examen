4041673f95e1dbc6b4bac81b65cbebaf661a3ac3
yahyamaw311 <yahyamaw311@gmail.com>
2024-09-16 15:20:06 -0400

    commit

--- examen/app/index.jsx
+++ examen/app/index.jsx
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to stargffddscsadsdfsdgft working on your app!</Text>
+      <Text>Open up App.js to start working on your app!</Text>
+      <Button title='testies' onPress={() => {console.log('testies has been clicked ' + (new Date().getMilliseconds()))}}></Button>
       <StatusBar style="auto" />
     </View>
   );
